d49ad6683e37624e867b750058134ff0202ae511
sky <sky.ilyashenko@gmail.com>
2016-06-10 22:34:59 +0300

    compromice in paginationItems (not observable but function)

--- js/main.js
+++ js/main.js
@@ -6,11 +6,6 @@ $(document).ready(function () {
 function koScope(){
 	var self = this;
 
-	// privet vars
-	var citiesData = {};
-	var filteredData = ko.observableArray([]);
-	//end of the privet vars
-
 	// Filtration tabs object
 	this.tabs = new (function(parent){
 		var self = this;
@@ -47,33 +42,31 @@ function koScope(){
 		]);
 		this.onClick = function(tabElement){
 			self.activeMethod(tabElement.title);
-			filteredData(self.filterData(citiesData, self.activeMethod()));
+			//filteredData(self.filterData(citiesData, self.activeMethod()));
 			parent.paginator.select(0);		// refresh paginator on start position
-		};
-		this.filterData = function(citiesData, tabsMethod){
-			if(tabsMethod === "все"){
-				return citiesData.items;
-			};
-			var filteredData = citiesData.items.filter(function(item){
-				return (item.continent === tabsMethod);
-			});
-			return filteredData;
 		};
 	})(this);	// end of Filtration tabs
 
-	// Pagination object
+		// Pagination object
 	this.paginator = new (function(parent){
 		var self = this;
 		this.firstCityNum = 0;
 		this.maxItemsOnPage = ko.observable(10);
 		this.activePaginationItem = ko.observable(0);
-		this.paginationItems = ko.computed(function(){
+		this.paginationItems = ko.observableArray();
+		this.updatePaginationItems = function () {
 			var res = [];
-			for(var i = 0; i < (filteredData().length/self.maxItemsOnPage()); i++){
-				res.push(i);
-			};
-			return res;
-		});
+			console.log(parent.outputData);
+			if(parent.outputData){
+				console.log(parent.outputData.citiesData().items);
+				for(var i = 0; i < (parent.outputData.citiesData().items.length/self.maxItemsOnPage()); i++){
+					res.push(i);
+				};
+			}else
+				res.push(0);
+
+			this.paginationItems(res);
+		};
 		this.width = ko.computed(function(){
 			return (self.paginationItems().length * 30 + 'px');
 		}, this);
@@ -84,29 +77,55 @@ function koScope(){
 			var lastItem = $(".pagecontainer div").last();
 			var contWrap = $(".pagecontainer_wrap");
 			var cont = $(".pagecontainer");
+
 			cont.css('left', '+=' + shift + "px");
 			if(cont.offset().left + shift > contWrap.offset().left)
-			    cont.css('left', 0 + "px");
+				cont.css('left', 0 + "px");
 			if(lastItem.offset().left + lastItem.width() + shift < contWrap.offset().left + contWrap.width())
 				cont.css('left',(cont.offset().left - lastItem.offset().left - lastItem.width() - 10 + contWrap.width()) + "px");
 		};
 		this.select = function(data, event){
 			self.activePaginationItem(data);
+			// shift calculation on jQuery
 			var targetPosition = $(".pagecontainer_wrap").offset().left + $(".pagecontainer_wrap").width()/2;
 			var activeItem = $($(".pagecontainer div")[self.activePaginationItem()]);
 			var shift = targetPosition - activeItem.offset().left;
+			// end of shift calculation
+
 			self.shiftPagContainer(shift - 20);
-			self.firstCityNum = self.activePaginationItem() * self.maxItemsOnPage();
-			parent.outputData.items( filteredData().slice(self.firstCityNum, self.firstCityNum + self.maxItemsOnPage()) );
 		};
 	})(this); // end of pagination
 
-	this.outputData = {
-		populationMin: ko.observable(100),
-		populationMax: ko.observable(25000000),
-		yearMin: ko.observable(0),
-		yearMax: ko.observable(2016),
-		items: ko.observableArray([])
+	this.outputData = new (function(parent){
+		var self = this;
+		this.populationMin = ko.observable(100);
+		this.populationMax = ko.observable(25000000);
+		this.yearMin = ko.observable(0);
+		this.yearMax = ko.observable(2016);
+		this.citiesData = ko.observable({
+			items: []
+		});
+		// todo: сделать нормальную filteredData2: ko.computed(function(){
+		this.filteredData2 = ko.computed(function(){
+			var filteredData = [];
+			if(parent.tabs.activeMethod() === "все"){
+				filteredData = self.citiesData().items;
+			}else{
+				filteredData = self.citiesData().items.filter(function(item){
+					return (item.continent === parent.tabs.activeMethod());
+				});
+			};
+			var paginatorObj = parent.paginator;
+			paginatorObj.firstCityNum = paginatorObj.activePaginationItem() * paginatorObj.maxItemsOnPage();
+			// todo: сделать нормальную filteredData2: ko.computed(function(){
+			if(filteredData)
+				return filteredData.slice(paginatorObj.firstCityNum, paginatorObj.firstCityNum + paginatorObj.maxItemsOnPage());
+		}, this)
+	})(this);
+
+	// submit from search form
+	self.searchForm = function(){
+		self.refreshData();
 	};
 
 	this.refreshData = function(){
@@ -118,17 +137,15 @@ function koScope(){
 			"yearMax": this.outputData.yearMax()
 		};
 		$.post('./backend/refreshData.php', data, function(response){
-			citiesData = response;
-			filteredData(self.tabs.filterData(citiesData, self.tabs.activeMethod()));
-			var paginatorObj = self.paginator;
-			self.outputData.items( filteredData().slice(paginatorObj.firstCityNum, paginatorObj.firstCityNum + paginatorObj.maxItemsOnPage()) );
+			// todo: проверка на наличие свойства items в response
+			self.outputData.citiesData(response);
+			self.paginator.updatePaginationItems();
+			// self.paginator.select(0);		// refresh paginator on start position
 		}, 'json');
 	};
 	this.refreshData();
 
-	// submit from search form
-	self.searchForm = function(){
-		self.refreshData();
-	};
+	// TODO: remove
+	window.THIS = this;
 
 } // end of koScope
